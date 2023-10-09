5fff5a2f90ecdea86c5ed413a12fac8cadc12e44
Thilagavathi Amulraj <Thilagavathi.Amulraj90@gmail.com>
2023-10-08 21:53:23 -0500

    Added requestBody server side validation

--- src/main/java/com/student/domain/StudentInfo.java
+++ src/main/java/com/student/domain/StudentInfo.java
@@ -1,5 +1,8 @@
 package com.student.domain;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,8 +19,12 @@ public class StudentInfo {
 
     @Id
     private String studentId;
+    @NotBlank(message = "studentInfo.firstName must be present")
     private String firstName;
+    @NotBlank(message = "studentInfo.lastName must be present")
     private String lastName;
+    @NotNull
+    @Positive(message = "studentInfo.age must be positive")
     private Integer age;
     private LocalDate dateOfBirth;
     private String grade;
